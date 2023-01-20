b92c2f517716a5b4fe2352f054b98984ddae4fc9
schmidtde-stout <schmidtde@uwstout.edu>
2023-01-20 11:17:38 -0600

    Update index.js
    another try

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Glitch!! -- From Local Push");
+  res.end("Hello Glitch!! -- From Local Push -- AGAIN");
 });
 
 server.listen(port, hostname, () => {
