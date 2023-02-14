30a865b2b69af8b7444978ae3d3a859a3966fc60
schmidtde-stout <schmidtde@uwstout.edu>
2023-02-13 19:38:34 -0600

    remove old code

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,6 @@ import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-/*
-class App extends Component {
-  [message] = useState("Nothing yet");
-  componentDidMount() {
-    this.callAPI()
-      .then((res) => this.setState({ message: res }))
-      .catch((err) => console.log(err));
-  }
-
-  callAPI = async () => {
-    const response = await fetch("http://localhost:5000/api");
-    const body = await response.json();
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    // note the body will come back looking like this -- { express: "simple response" }
-    return body.express; // this is returned in the "response" to a .then() promise chain
-  };
-
-
-*/
-
 function App() {
   const [message, setMessage] = useState("Nothing Yet");
   const [error, setError] = useState(null);
